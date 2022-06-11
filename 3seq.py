4501614b7fde777d23cefd45a2727eedcd37d635
tihhor <tihhor@gmail.com>
2022-06-11 23:31:27 +0200

    dz03

--- 3seq.py
+++ 3seq.py
@@ -3,5 +3,10 @@
 my_list1 = input('Введите список 1 через запятую: ').replace('/',',').replace(';',',').split(',')
 my_list2 = input('Введите список 2 через запятую: ').replace('/',',').replace(';',',').split(',')
 
-my_set_diff = set(my_list1) - set(my_list2)
+#my_set_diff = set(my_list1) - set(my_list2)
+my_set_diff = []
+for item in my_list1:
+    if item not in my_list2:
+        my_set_diff.append(item)
+
 print('Результат: ', my_set_diff)
